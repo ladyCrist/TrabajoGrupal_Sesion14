89fd2d196be311a35ae1fb3927794ce5e5a92a36
ladyCrist <116976078+ladyCrist@users.noreply.github.com>
2022-11-08 02:46:18 -0500

    Add files via upload

---
+++ S14/funciones.js
@@ -0,0 +1,34 @@
+function datos1() {
+    document.getElementById('nombre').innerHTML = 'Lady';
+    document.getElementById('apellidos').innerHTML = 'Espinoza Aguedo';
+    document.getElementById('celular').innerHTML = '931010430';
+    document.getElementById('email').innerHTML = 'elady386@gmail.com';
+}
+
+function datos2() {
+    document.getElementById('nombre').innerHTML = 'Leya';
+    document.getElementById('apellidos').innerHTML = 'Delgado Escobar';
+    document.getElementById('celular').innerHTML = '962414282';
+    document.getElementById('email').innerHTML = 'redestanday09@gmail.com';
+}
+
+function datos3() {
+    document.getElementById('nombre').innerHTML = 'Luis';
+    document.getElementById('apellidos').innerHTML = 'Escobedo Velasquez';
+    document.getElementById('celular').innerHTML = '901833716';
+    document.getElementById('email').innerHTML = 'lescobedo.im@gmail.com';
+}
+
+function datos4() {
+    document.getElementById('nombre').innerHTML = 'Nerio';
+    document.getElementById('apellidos').innerHTML = 'Cañari Huarcaya';
+    document.getElementById('celular').innerHTML = '910426974';
+    document.getElementById('email').innerHTML = 'huarcayanerio@gmail.com';
+}
+
+function datos5() {
+    document.getElementById('nombre').innerHTML = 'Rut Patricia';
+    document.getElementById('apellidos').innerHTML = 'Condori Obregon';
+    document.getElementById('celular').innerHTML = '927 121 915';
+    document.getElementById('email').innerHTML = 'ccondori.patricia@gmail.com';
+}
